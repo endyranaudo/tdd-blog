77d855b88b36555a535a2223e9ad3f456e3073f9
Endy Ranaudo <endy@wearesnook.com>
2020-08-25 15:12:57 +0100

    fixed errors. Next step is to implement Devise

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,11 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  context "GET #index" do
+  describe "GET #index" do
     it "has a 200 status code" do
-      get :index
+      get users_path
       expect(response.status).to eq(200)
+    end
+
+    it "should render the users#index template" do
+      # Given - Preconditions
+      # When - Action
+      get users_path
+      # Then - Result
+      expect(response).to render_template(:index)
     end
   end
 
+  describe "GET #show" do
+    it "should be return a 200 status" do
+      user = User.create! username: "Endy", email: "endy@wearesnook.com", password: "insecurepassword"
+      get user_url user
+      expect(response).to be_successful
+    end
+  end
 end
