1a7e4b8d254d2ea7c4b7146c21ca5de817262e8c
Endy Ranaudo <endy@wearesnook.com>
2020-09-10 10:14:55 +0100

    Edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root to: 'home#index'
+  # PROTECTED ROUTES
+  authenticate :user do
+    resources :posts, only: [:new, :create, :edit, :update, :destroy]
+  end
   resources :users, only: [:index, :show]
   resources :posts
 end
